8c218d2d1c9b8d898e66ee6280ce2cf6ee3a32a4
Tomilov Sergey <tomexpert@yandex.ru>
2021-06-21 15:19:02 +0800

    Оптимизация loginPage.js

--- public/loginPage.js
+++ public/loginPage.js
@@ -4,21 +4,7 @@ let netUser = new UserForm();
 netUser.loginFormCallback = (data) => ApiConnector.login(data, answerLogin);
 netUser.registerFormCallback = (data) => ApiConnector.register(data, answerRegistrator);
 
-function answerLogin(result) {
-  if (result.success === true) {
-    location.reload();
-  } else {
-    netUser.setLoginErrorMessage(result.error);
-  }
-}
-
-function answerRegistrator(result) {
-  if (result.success === true) {
-    location.reload();
-  } else {
-    netUser.setRegisterErrorMessage(result.error);
-  }
-}
-
+const answerLogin = (result) => result.success === true ? location.reload() : netUser.setLoginErrorMessage(result.error);
+const answerRegistrator = (result) => result.success === true ? location.reload() : netUser.setLoginErrorMessage(result.error);
 
 
