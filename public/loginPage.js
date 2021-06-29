91c5e9d40765d077580e389b622fa779f83f3906
Tomilov Sergey <tomexpert@yandex.ru>
2021-06-29 14:43:36 +0800

    Испарвлено обявление переменных в loginPage.js

--- public/loginPage.js
+++ public/loginPage.js
@@ -1,10 +1,12 @@
-'use strict'
+//'use strict'
 //Реализация страницы «Вход и регистрация»
 let netUser = new UserForm();
-netUser.loginFormCallback = (data) => ApiConnector.login(data, answerLogin);
-netUser.registerFormCallback = (data) => ApiConnector.register(data, answerRegistrator);
 
 const answerLogin = (result) => result.success === true ? location.reload() : netUser.setLoginErrorMessage(result.error);
 const answerRegistrator = (result) => result.success === true ? location.reload() : netUser.setLoginErrorMessage(result.error);
+
+netUser.loginFormCallback = (data) => ApiConnector.login(data, answerLogin);
+netUser.registerFormCallback = (data) => ApiConnector.register(data, answerRegistrator);
+
 
 
